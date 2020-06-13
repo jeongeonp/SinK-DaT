e0f48b8c499c00cae4e09158d777f6f36d232782
Hyunchang <hyunchang@kaist.ac.kr>
2020-06-14 03:20:59 +0900

    temp

--- news-corpus-new.py
+++ news-corpus-new.py
@@ -6,11 +6,13 @@ from urllib import request
 from bs4 import BeautifulSoup
 import pandas as pd
 import random
+import pickle
 
 
 def getArticle(year, month, day):
     print("===== Searching [" + str(year) + "-" + str(month) + "-" + str(day) + "] =====\n")
     url = "http://srchdb1.chosun.com/pdf/i_archive/index.jsp?Y="+str(year)+"&M="+str(month)+"&D="+str(day)
+    print(url)
     soup = BeautifulSoup(request.urlopen(url).read(), 'html.parser')
     res = soup.find_all('table')
 
@@ -46,6 +48,10 @@ def getArticle(year, month, day):
 
 
 # Year: 1990~, Month: 1-12, Day: 1-30 or 1-31
-result = getArticle(2005, 6, 14)
+year = [1990,1991,1992,1993,1994,1995]
+new_year = [2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020]
+
+
+result = getArticle(2005, 6, 15)
 
 print(result)
