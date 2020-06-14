4e5cfc73c366a034ee5f90e0243d3e6a0af85152
Hyunchang <hyunchang@kaist.ac.kr>
2020-06-14 19:53:24 +0900

    add assertion for counter.py

--- counter.py
+++ counter.py
@@ -8,12 +8,12 @@ def get_counter(filename:str)->Counter:
     all_hanjas = Counter()
 
     for key in data["annotated"]:
-        assert data[key][0][1]
+        assert isinstance(data[key][0][1],list), "list of annotated keys corrupted"
         for sentence in data[key]:
             all_hanjas += Counter(sentence[1])
 
     return all_hanjas
 
-print(get_counter("old.pkl"))
+
 
 
